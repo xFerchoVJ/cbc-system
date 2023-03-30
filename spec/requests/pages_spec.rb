eeea23bdc651d643e4a7f2a1b0ae09134a32e929
xFerchoVJ <fer_elsword@hotmail.com>
2023-03-30 12:27:02 -0600

    Add navbar and carousel

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /Home" do
+  describe "GET /" do
     it "returns http success" do
-      get home_path
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
