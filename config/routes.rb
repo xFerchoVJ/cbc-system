81d63773518aab929a770e2376ecfabdb4a63f69
xFerchoVJ <fer_elsword@hotmail.com>
2023-03-30 10:33:21 -0600

    Add RSpec, and some specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get '/home', to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
